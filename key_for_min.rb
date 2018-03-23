e50d9320f45d093c68f7ff2f34ee2c43fbaaeb43
Megan Collier <meganraec@live.com>
2018-03-23 04:29:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.reduce {|k, v| k.last > v.last ? v : k }.first
   if name_hash = {}
     nil
   end
+  name_hash.reduce {|k, v| k.last > v.last ? v : k }.first
 # # name_hash.reduce do |k, v|
 # #   if k.last > v.last
 # #     v
