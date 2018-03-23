f4e76cd40dac53c7b95f3df77d85168beeae3393
Megan Collier <meganraec@live.com>
2018-03-23 04:24:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   # name_hash.reduce {|k, v| k.last > v.last ? v : k }.first
 name_hash.reduce do |k, v|
   if k.last > v.last
-    v.first
+    v
   elsif k.last < v.last
-    k.first
+    k
   else name_hash = {}
     nil
 end
